aa445dee04188ea58690e538974d0df8efdcf2ae
Smak80 <smak-80@yandex.ru>
2019-12-05 00:31:52 +0300

    Добавьте файлы проекта.

---
+++ SuperSimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SuperSimpleCalculator
+{
+    partial class CalcForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNum1 = new System.Windows.Forms.TextBox();
+            this.tbNum2 = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnTimes = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Число 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Число 2:";
+            // 
+            // tbNum1
+            // 
+            this.tbNum1.Location = new System.Drawing.Point(92, 6);
+            this.tbNum1.Name = "tbNum1";
+            this.tbNum1.Size = new System.Drawing.Size(358, 26);
+            this.tbNum1.TabIndex = 2;
+            this.tbNum1.Text = "0";
+            // 
+            // tbNum2
+            // 
+            this.tbNum2.Location = new System.Drawing.Point(92, 38);
+            this.tbNum2.Name = "tbNum2";
+            this.tbNum2.Size = new System.Drawing.Size(358, 26);
+            this.tbNum2.TabIndex = 3;
+            this.tbNum2.Text = "0";
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.AutoSize = true;
+            this.btnPlus.Location = new System.Drawing.Point(16, 83);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 30);
+            this.btnPlus.TabIndex = 4;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.operation_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 132);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(97, 20);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Результат: ";
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.AutoSize = true;
+            this.btnMinus.Location = new System.Drawing.Point(135, 83);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(75, 30);
+            this.btnMinus.TabIndex = 6;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.operation_Click);
+            // 
+            // btnTimes
+            // 
+            this.btnTimes.AutoSize = true;
+            this.btnTimes.Location = new System.Drawing.Point(254, 83);
+            this.btnTimes.Name = "btnTimes";
+            this.btnTimes.Size = new System.Drawing.Size(75, 30);
+            this.btnTimes.TabIndex = 7;
+            this.btnTimes.Text = "*";
+            this.btnTimes.UseVisualStyleBackColor = true;
+            this.btnTimes.Click += new System.EventHandler(this.operation_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.AutoSize = true;
+            this.btnDiv.Location = new System.Drawing.Point(373, 83);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(75, 30);
+            this.btnDiv.TabIndex = 8;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.operation_Click);
+            // 
+            // CalcForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 161);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnTimes);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.tbNum2);
+            this.Controls.Add(this.tbNum1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximumSize = new System.Drawing.Size(478, 200);
+            this.MinimumSize = new System.Drawing.Size(478, 200);
+            this.Name = "CalcForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Простенький калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNum1;
+        private System.Windows.Forms.TextBox tbNum2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnTimes;
+        private System.Windows.Forms.Button btnDiv;
+    }
+}
+
---
+++ SuperSimpleCalculator/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperSimpleCalculator
+{
+    public partial class CalcForm : Form
+    {
+        public CalcForm()
+        {
+            InitializeComponent();
+        }
+
+        private void operation_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            try
+            {
+                a = double.Parse(tbNum1.Text);
+            } catch (Exception ex)
+            {
+                a = 0.0;
+            }
+            try
+            {
+                b = double.Parse(tbNum2.Text);
+            } catch (Exception ex)
+            {
+                b = 0.0;
+            }
+
+            double c = 0;
+            if (((Button) sender).Name == "btnPlus")
+            {
+                c = a + b;
+            } else if (((Button) sender).Name == "btnMinus")
+            {
+                c = a - b;
+            } else if (((Button) sender).Name == "btnTimes")
+            {
+                c = a * b;
+            } else if (((Button) sender).Name == "btnDiv")
+            {
+                c = a / b;
+            }
+
+            lblResult.Text = "Результат: " + c;
+        }
+    }
+}
